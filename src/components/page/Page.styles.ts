085198396481f6955a4911334df3468e2d81df85
HugoBosy <hubert.kliszcz@gmail.com>
2022-10-27 10:28:47 +0200

    add background image

--- src/components/page/Page.styles.ts
+++ src/components/page/Page.styles.ts
@@ -1,8 +1,11 @@
 import styled from "styled-components";
+import image from '../../ui/img/miles.jpg';
 
 const Wrapper = styled.div`
   width: 100%;
   height: 100vh;
+  background-image: url(${image});
+  background-size: cover;
 `;
 
 export default { Wrapper };
